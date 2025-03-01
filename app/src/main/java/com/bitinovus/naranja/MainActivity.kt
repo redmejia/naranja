724cd4d05f5ad390ca48cca4c4c5f7a8eb00f343
redmejia <reynaldoemejia@gmail.com>
2025-03-01 14:53:51 -0800

    API config, Image container and stack

--- app/src/main/java/com/bitinovus/naranja/MainActivity.kt
+++ app/src/main/java/com/bitinovus/naranja/MainActivity.kt
@@ -1,28 +1,41 @@
 package com.bitinovus.naranja
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.animation.core.Animatable
+import androidx.compose.foundation.gestures.Orientation
+import androidx.compose.foundation.gestures.draggable
+import androidx.compose.foundation.gestures.rememberDraggableState
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.graphics.CompositingStrategy
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.unit.IntOffset
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
-import coil.compose.AsyncImage
+import com.bitinovus.naranja.presentation.ui.components.stack.StackContainer
+import com.bitinovus.naranja.presentation.ui.components.stack.StackImage
 import com.bitinovus.naranja.presentation.viewmodels.profileviewmodel.ProfileViewModel
 import com.bitinovus.naranja.ui.theme.NaranjaTheme
+import kotlinx.coroutines.launch
+import kotlin.math.abs
 
 class MainActivity : ComponentActivity() {
     private lateinit var profileViewModel: ProfileViewModel
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -35,36 +48,89 @@ class MainActivity : ComponentActivity() {
                 val profileState by profileViewModel.profileState.collectAsState()
 
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    LazyColumn (
-                        modifier = Modifier.padding(innerPadding),
-                    ){
-                        items(items = profileState.data) { profile ->
-                            AsyncImage(
-                                modifier = Modifier.fillMaxSize(),
-                                contentScale = ContentScale.Crop,
-                                model = profile.image,
-                                contentDescription = null
-                            )
+
+                    Box(
+                        modifier = Modifier
+                            .padding(innerPadding)
+                            .fillMaxWidth()
+                            .fillMaxSize()
+                            .graphicsLayer { compositingStrategy = CompositingStrategy.Offscreen }
+                    ) {
+                        var offset = 0.dp
+
+                        for (profile in profileState.data) {
+
+                            val screenHeight = LocalConfiguration.current.screenHeightDp.toFloat()
+                            val scope = rememberCoroutineScope()
+                            val translationY = remember { Animatable(0f) }
+                            translationY.updateBounds(-screenHeight, screenHeight)
+
+                            val draggableState = rememberDraggableState { delta ->
+                                scope.launch {
+                                    translationY.snapTo(translationY.value + delta)
+                                }
+                            }
+
+                            StackContainer(
+                                modifier = Modifier
+                                    .offset(y = offset) // image offset
+                                    .offset { // animation offset
+                                        IntOffset(x = 0, y = translationY.value.toInt())
+                                    }
+                                    .draggable(
+                                        state = draggableState,
+                                        orientation = Orientation.Vertical,
+                                        onDragStopped = { velocity ->
+                                            val follow =
+                                                translationY.value < -screenHeight / 3f
+                                            val remove = translationY.value > screenHeight / 3f
+
+                                            scope.launch {
+                                                when {
+                                                    follow -> {
+                                                        translationY.animateTo(
+                                                            -screenHeight,
+                                                            initialVelocity = velocity,
+                                                        )
+                                                        Log.d(
+                                                            "ACTION",
+                                                            "onCreate: ADD"
+                                                        )
+                                                        // TEST
+                                                        profileViewModel.delete(profile)
+                                                    }
+
+                                                    remove -> {
+                                                        translationY.animateTo(
+                                                            screenHeight,
+                                                            initialVelocity = velocity,
+                                                        )
+                                                        Log.d(
+                                                            "ACTION",
+                                                            "onCreate: REMOVE"
+                                                        )
+                                                        // TEST
+                                                        profileViewModel.delete(profile)
+                                                    }
+
+                                                    else -> {
+                                                        translationY.animateTo(0f)
+                                                    }
+                                                }
+                                            }
+                                        }
+                                    )
+                                    .graphicsLayer {
+                                        alpha = 1f - (abs(translationY.value) / screenHeight)
+                                    }
+                            ) {
+                                StackImage(profile = profile)
+                            }
+                            offset += 25.dp / 2
                         }
                     }
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    NaranjaTheme {
-        Greeting("Android")
     }
 }
